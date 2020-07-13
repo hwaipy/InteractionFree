a021a92a03ae1a0eeda4b1633adf840af1d2143d
Hwaipy Li <hwaipy@gmail.com>
2020-07-13 17:47:50 +0800

    abstract TDCStorageStreamFatcher into IFExp

--- InteractionFreePlatform/app/mdiqkd_tdcviewer/mdiqkd_tdcviewer.js
+++ InteractionFreePlatform/app/mdiqkd_tdcviewer/mdiqkd_tdcviewer.js
@@ -9,104 +9,36 @@ $(document).ready(function() {
     }
   }
   collection = parameters['collection'] || "TDCLocalTest"
-  setFetchingProgress(0.0)
+  // setFetchingProgress(0.0)
 
   worker = new IFWorker("ws://" + window.location.host + "/ws", function() {
-    viewerFetcher = new TDCViewerStorageStreamFatcher(worker,
-      collection)
-    viewerFetcher.start(500)
+    fetcher = new TDCStorageStreamFatcher(worker, collection, 500, {
+      'Data.Counter': 1,
+      'Data.MultiHistogram': 1
+    }, plot, listener)
+    fetcher.start()
   })
 });
 
-class TDCViewerStorageStreamFatcher {
-  constructor(worker, collection) {
-    this.worker = worker
-    this.collection = collection
-    this.lastTime = null
-      // modes: Instant, Integral, IntegralContinues
-    this.mode = 'Instant'
-    this.filter = {
-      'Data.Counter': 1,
-      'Data.MultiHistogram': 1
-    }
-    this.fetchID = 0
-    this.integralTime = 0
-    this.integralBeginTime = null
-    this.integralFetchedDataCount = 0
-    this.integralTotalDataCount = 0
-    this.integralContinuesHasNew = false
-    this.clearPlot()
+function plot(result, append) {
+  var layout = {
+    xaxis: {
+      title: 'Time (ns)'
+    },
+    yaxis: {
+      title: 'Count'
+    },
   }
-
-  start(interval) {
-    this.update()
-    setInterval("viewerFetcher.update()", '' + interval)
-  }
-
-  update() {
-    if (this.mode == 'Instant' || this.mode == 'IntegralContinues') {
-      var fetchID = this.fetchID
-      worker.request("Storage", "latest", [this.collection, this.lastTime,
-          this.filter
-        ], {},
-        function(result) {
-          if (fetchID == viewerFetcher.fetchID) {
-            if (result != null) {
-              viewerFetcher.lastTime = result['FetchTime'];
-              if (viewerFetcher.mode == 'Instant' ||
-                viewerFetcher.mode == 'IntegralContinues') {
-                viewerFetcher.updateResult(result, fetchID, 'update')
-              }
-            }
-            if (viewerFetcher.mode == 'IntegralContinues') {
-              viewerFetcher.integralTime = parseInt((new Date().getTime() - viewerFetcher.integralBeginTime.getTime())/1000)
-              if (result != null){
-                viewerFetcher.integralTotalDataCount += 1
-                viewerFetcher.integralFetchedDataCount += 1
-              }
-            }
-            viewerFetcher.updateFetchingInfo()
-          }
-        },
-        function(error) {
-          console.log("Error: " + error)
-        }
-      )
-    }
-    viewerFetcher.updateFetchingInfo()
-  }
-
-  range(beginTime, endTime) {
-    var fetchID = this.fetchID
-    this.integralTime = parseInt((endTime.getTime() - beginTime.getTime())/1000)
-    worker.request("Storage", "range", [this.collection, dateToISO(beginTime), dateToISO(endTime),
-        'FetchTime', this.filter
-      ], {},
-      function(result) {
-        viewerFetcher.integralTotalDataCount += result.length
-        for (var i = 0; i < result.length; i++) {
-          worker.request("Storage", "get", [viewerFetcher.collection,
-              result[i]['_id'], viewerFetcher.filter
-            ], {},
-            function(result) {
-              if (fetchID == viewerFetcher.fetchID) {
-                viewerFetcher.updateResult(result)
-                viewerFetcher.integralFetchedDataCount += 1
-                viewerFetcher.updateFetchingInfo()
-              }
-            },
-            function(error) {
-              console.log("Error: " + error)
-            })
-        }
-      },
-      function(error) {
-        console.log("Error: " + error)
-      }
-    )
-  }
-
-  updateResult(result) {
+  if (result == null) {
+    var traces = [{
+      x: [0],
+      y: [0],
+      type: 'scatter',
+      name: 'CH0'
+    }]
+    $('#HistogramWarning')[0].classList.add('d-none')
+  } else {
+    console.log(append);
     var data = result['Data']['MultiHistogram']
     var configuration = data['Configuration']
     var histograms = data['Histograms']
@@ -116,8 +48,7 @@ class TDCViewerStorageStreamFatcher {
     var length = configuration['BinCount'];
     var syncChannel = configuration['Sync'];
     var signalChannels = configuration['Signals'];
-    var xs = linspace(viewFrom, viewTo / divide,
-      length)
+    var xs = linspace(viewFrom, viewTo / divide, length)
     var traces = []
     for (var i = 0; i < signalChannels.length; i++) {
       var channelNum = signalChannels[i].toString()
@@ -129,85 +60,12 @@ class TDCViewerStorageStreamFatcher {
         name: 'CH' + channelNum
       })
     }
-    var layout = {
-      xaxis: {
-        title: 'Time (ns)'
-      },
-      yaxis: {
-        title: 'Count'
-      },
-      uirevision:'true',
-    };
-    Plotly.react('viewport', traces, layout, {
-      displaylogo: false,
-      // responsive: true
-    });
+    layout['uirevision'] = 'true'
   }
-
-  clearPlot() {
-    var traces = [{
-      x: [0],
-      y: [0],
-      type: 'scatter',
-      name: 'CH0'
-    }]
-    var layout = {
-      xaxis: {
-        title: 'Time (ns)'
-      },
-      yaxis: {
-        title: 'Count'
-      }
-    };
-    Plotly.react('viewport', traces, layout, {
-      displaylogo: false,
-      // responsive: true
-    });
-    $('#HistogramWarning')[0].classList.add('d-none')
-  }
-
-  updateFetchingInfo(){
-    if (this.mode == 'Instant' || (this.mode == 'IntegralContinues' && this.integralContinuesHasNew)){
-      $('#HistogramWarning')[0].classList.add('d-none')
-      var fetchTimeDelta = new Date().getTime() - Date.parse(this.lastTime)
-      if (fetchTimeDelta > 3000) {
-        $('#HistogramWarning')[0].classList.remove('d-none')
-        $('#HistogramWarningContent').html("The most recent data was fetched " + parseInt(fetchTimeDelta / 1000) + " s ago.")
-      }
-    }
-    if (this.integralTotalDataCount > 0 && this.integralFetchedDataCount < this.integralTotalDataCount){
-      setFetchingProgress(this.integralFetchedDataCount * 1.0 / this.integralTotalDataCount)
-    } else {
-      setFetchingProgress(0.0)
-    }
-    if (this.mode == 'Integral' || this.mode == 'IntegralContinues'){
-      var content = this.integralTotalDataCount + ' (' + this.integralTime + ' s)'
-      if (this.integralFetchedDataCount < this.integralTotalDataCount) {
-        content = this.integralFetchedDataCount + '/' + content
-      }
-      $('#FetchNumber')[0].classList.remove('d-none')
-      $('#FetchNumberContent').html(content)
-    } else {
-      $('#FetchNumberContent').html('')
-      $('#FetchNumber')[0].classList.add('d-none')
-    }
-  }
-
-  changeMode(mode) {
-    if (mode != 'Instant' && mode != 'Integral' && mode !=
-      'IntegralContinues' && mode != 'Stop') {
-      console.log('Bad mode: ' + mode)
-      return
-    }
-    this.mode = mode
-    this.integralTime = 0
-    this.integralTotalDataCount = 0
-    this.integralFetchedDataCount = 0
-    this.integralContinuesHasNew = false
-    if (mode == 'Instant') this.lastTime = 0
-    if (mode != 'Stop') this.fetchID += 1
-    this.clearPlot()
-  }
+  Plotly.react('viewport', traces, layout, {
+    displaylogo: false,
+    // responsive: true
+  })
 }
 
 function updateIntegralData() {
@@ -217,12 +75,7 @@ function updateIntegralData() {
     $("#input-integral-to")[0].classList.contains('is-invalid')
   var isToNow = $("#input-integral-to")[0].value
   var isToNow = isToNow.length == 0 || isToNow.toLowerCase() == 'now'
-  if (!invalid) {
-    viewerFetcher.integralBeginTime = beginTime
-    viewerFetcher.changeMode(isToNow ? "IntegralContinues" : "Integral")
-    if(isToNow) viewerFetcher.lastTime = dateToISO(endTime)
-    viewerFetcher.range(beginTime, endTime)
-  }
+  if (!invalid) fetcher.updateIntegralData(beginTime, endTime, isToNow)
 }
 
 function onSelectionIntegral(isIntegral) {
@@ -231,7 +84,7 @@ function onSelectionIntegral(isIntegral) {
   $("#selection-integral").attr("class", isIntegral ? "btn btn-success" :
     "btn btn-secondary")
   $("#IntegralConfig").collapse(isIntegral ? "show" : "hide")
-  viewerFetcher.changeMode(isIntegral ? "Stop" : "Instant")
+  fetcher.changeMode(isIntegral ? "Stop" : "Instant")
 }
 
 function onBlurIntegralRange(id) {
@@ -252,11 +105,359 @@ function onBlurIntegralRange(id) {
   return parsedDate
 }
 
-function dateToISO(date) {
-  return dateToString(date).replace(' ', 'T') + '.000000+08:00'
+function listener(event, arg) {
+  if (event == 'FetchTimeDelta') {
+    fetchTimeDelta = arg
+    if (fetchTimeDelta > 3000) {
+      $('#HistogramWarning')[0].classList.remove('d-none')
+      $('#HistogramWarningContent').html("The most recent data was fetched " + parseInt(fetchTimeDelta / 1000) + " s ago.")
+    } else {
+      $('#HistogramWarning')[0].classList.add('d-none')
+    }
+  } else if (event == 'FetchingProgress') {
+    progress = parseInt(arg * 100)
+    $('#FetchingProgress').attr('style', 'background-image: linear-gradient(to right, #BDE6FF ' + (progress) + '%, #F8F9FC ' + (progress) + '%)')
+  } else if (event == 'FetchingNumber') {
+    if (arg == null) {
+      $('#FetchNumberContent').html('')
+      $('#FetchNumber')[0].classList.add('d-none')
+    } else {
+      integralFetchedDataCount = arg[0]
+      integralTotalDataCount = arg[1]
+      integralTime = arg[2]
+      content = integralTotalDataCount + ' items (in ' + integralTime + ' s)'
+      if (integralFetchedDataCount < integralTotalDataCount) content = integralFetchedDataCount + ' / ' + content
+      $('#FetchNumber')[0].classList.remove('d-none')
+      $('#FetchNumberContent').html(content)
+    }
+  } else {
+    console.log(event + ', '+ arg);
+  }
 }
 
-function setFetchingProgress(p) {
-  progress = parseInt(p * 100)
-  $('#FetchingProgress').attr('style', 'background-image: linear-gradient(to right, #BDE6FF ' + (progress) + '%, #F8F9FC ' + (progress) + '%)')
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class TDCViewerStorageStreamFatcher {
+//   constructor(worker, collection) {
+//     this.worker = worker
+//     this.collection = collection
+//     this.lastTime = null
+//       // modes: Instant, Integral, IntegralContinues
+//     this.mode = 'Instant'
+//     this.filter = {
+//       'Data.Counter': 1,
+//       'Data.MultiHistogram': 1
+//     }
+//     this.fetchID = 0
+//     this.integralTime = 0
+//     this.integralBeginTime = null
+//     this.integralEndTime = null
+//     this.integralMostRecentTime = null
+//     this.integralFetchedDataCount = 0
+//     this.integralTotalDataCount = 0
+//     this.integralContinuesHasNew = false
+//     this.clearPlot()
+//   }
+//
+//   start(interval) {
+//     this.update()
+//     setInterval("viewerFetcher.update()", '' + interval)
+//   }
+//
+//   update() {
+//     if (this.mode == 'Instant' || this.mode == 'IntegralContinues') {
+//       var fetchID = this.fetchID
+//       worker.request("Storage", "latest", [this.collection, this.lastTime, this.filter], {},
+//         function(result) {
+//           if (fetchID == viewerFetcher.fetchID) {
+//             if (result != null) {
+//               viewerFetcher.lastTime = result['FetchTime'];
+//               if (viewerFetcher.mode == 'Instant' || viewerFetcher.mode == 'IntegralContinues') {
+//                 viewerFetcher.updateResult(result, fetchID, 'update')
+//               }
+//             }
+//             if (viewerFetcher.mode == 'IntegralContinues') {
+//               viewerFetcher.integralTime = parseInt((new Date().getTime() - viewerFetcher.integralBeginTime.getTime())/1000)
+//               if (result != null){
+//                 viewerFetcher.integralTotalDataCount += 1
+//                 viewerFetcher.integralFetchedDataCount += 1
+//               }
+//             }
+//             viewerFetcher.updateFetchingInfo()
+//           }
+//         },
+//         function(error) {
+//           console.log("Error: " + error)
+//         }
+//       )
+//     }
+//     viewerFetcher.updateFetchingInfo()
+//   }
+//
+//   range(beginTime, endTime) {
+//     var fetchID = this.fetchID
+//     this.integralTime = parseInt((endTime.getTime() - beginTime.getTime())/1000)
+//     worker.request("Storage", "range", [this.collection, dateToISO(beginTime), dateToISO(endTime), 'FetchTime', this.filter], {},
+//       function(result) {
+//         viewerFetcher.integralTotalDataCount += result.length
+//         for (var i = 0; i < result.length; i++) {
+//           worker.request("Storage", "get", [viewerFetcher.collection, result[i]['_id'], viewerFetcher.filter], {},
+//             function(result) {
+//               if (fetchID == viewerFetcher.fetchID) {
+//                 viewerFetcher.updateResult(result)
+//                 viewerFetcher.integralFetchedDataCount += 1
+//                 viewerFetcher.updateFetchingInfo()
+//               }
+//             },
+//             function(error) {
+//               console.log("Error: " + error)
+//             })
+//           viewerFetcher.integralMostRecentTime = new Date();
+//           viewerFetcher.integralMostRecentTime.setTime(Date.parse(result[i]['FetchTime']))
+//           console.log(viewerFetcher.integralMostRecentTime);
+//         }
+//       },
+//       function(error) {
+//         console.log("Error: " + error)
+//       }
+//     )
+//   }
+//
+//   updateResult(result) {
+//     var data = result['Data']['MultiHistogram']
+//     var configuration = data['Configuration']
+//     var histograms = data['Histograms']
+//     var viewFrom = configuration['ViewStart'] / 1000.0;
+//     var viewTo = configuration['ViewStop'] / 1000.0;
+//     var divide = configuration['Divide'];
+//     var length = configuration['BinCount'];
+//     var syncChannel = configuration['Sync'];
+//     var signalChannels = configuration['Signals'];
+//     var xs = linspace(viewFrom, viewTo / divide, length)
+//     var traces = []
+//     for (var i = 0; i < signalChannels.length; i++) {
+//       var channelNum = signalChannels[i].toString()
+//       if (channelNum.length == 1) channelNum = "0" + channelNum
+//       traces.push({
+//         x: xs,
+//         y: histograms[i],
+//         type: 'scatter',
+//         name: 'CH' + channelNum
+//       })
+//     }
+//     var layout = {
+//       xaxis: {
+//         title: 'Time (ns)'
+//       },
+//       yaxis: {
+//         title: 'Count'
+//       },
+//       uirevision:'true',
+//     };
+//     Plotly.react('viewport', traces, layout, {
+//       displaylogo: false,
+//       // responsive: true
+//     });
+//   }
+//
+//   clearPlot() {
+//     var traces = [{
+//       x: [0],
+//       y: [0],
+//       type: 'scatter',
+//       name: 'CH0'
+//     }]
+//     var layout = {
+//       xaxis: {
+//         title: 'Time (ns)'
+//       },
+//       yaxis: {
+//         title: 'Count'
+//       }
+//     };
+//     Plotly.react('viewport', traces, layout, {
+//       displaylogo: false,
+//       // responsive: true
+//     });
+//     $('#HistogramWarning')[0].classList.add('d-none')
+//   }
+//
+//   updateFetchingInfo() {
+//     // Check if display no data warning
+//     var fetchTimeDelta = 0
+//     if (this.mode == 'Instant') {
+//       fetchTimeDelta = new Date().getTime() - Date.parse(this.lastTime)
+//     } else if (this.mode == 'IntegralContinues') {
+//       if (this.integralContinuesHasNew) {
+//         fetchTimeDelta = new Date().getTime() - Date.parse(this.lastTime)
+//       } else {
+//         if (this.integralFetchedDataCount == this.integralTotalDataCount && this.integralMostRecentTime != null) {
+//           fetchTimeDelta = new Date().getTime() - this.integralMostRecentTime.getTime()
+//         }
+//       }
+//     }
+//     if (this.mode == 'Integral' || this.mode == 'IntegralContinues') {
+//       if (this.integralFetchedDataCount == 0) {
+//         fetchTimeDelta = 0
+//       }
+//     }
+//     if (fetchTimeDelta > 3000) {
+//       $('#HistogramWarning')[0].classList.remove('d-none')
+//       $('#HistogramWarningContent').html("The most recent data was fetched " + parseInt(fetchTimeDelta / 1000) + " s ago.")
+//     } else {
+//       $('#HistogramWarning')[0].classList.add('d-none')
+//     }
+//
+//     // Set prograss
+//     if (this.integralTotalDataCount > 0 && this.integralFetchedDataCount < this.integralTotalDataCount) {
+//       setFetchingProgress(this.integralFetchedDataCount * 1.0 / this.integralTotalDataCount)
+//     } else {
+//       setFetchingProgress(0.0)
+//     }
+//
+//     // Set FetchNumber
+//     if (this.mode == 'Integral' || this.mode == 'IntegralContinues') {
+//       var content = this.integralTotalDataCount + ' items (in ' + this.integralTime + ' s)'
+//       if (this.integralFetchedDataCount < this.integralTotalDataCount) {
+//         content = this.integralFetchedDataCount + '/' + content
+//       }
+//       $('#FetchNumber')[0].classList.remove('d-none')
+//       $('#FetchNumberContent').html(content)
+//     } else {
+//       $('#FetchNumberContent').html('')
+//       $('#FetchNumber')[0].classList.add('d-none')
+//     }
+//   }
+//
+//   changeMode(mode) {
+//     if (mode != 'Instant' && mode != 'Integral' && mode !=
+//       'IntegralContinues' && mode != 'Stop') {
+//       console.log('Bad mode: ' + mode)
+//       return
+//     }
+//     this.mode = mode
+//     this.integralTime = 0
+//     this.integralTotalDataCount = 0
+//     this.integralFetchedDataCount = 0
+//     this.integralContinuesHasNew = false
+//     if (mode == 'Instant') this.lastTime = 0
+//     if (mode != 'Stop') this.fetchID += 1
+//     this.clearPlot()
+//   }
+// }
+//
+// function updateIntegralData() {
+//   var beginTime = onBlurIntegralRange('input-integral-from')
+//   var endTime = onBlurIntegralRange('input-integral-to')
+//   invalid = $("#input-integral-from")[0].classList.contains('is-invalid') ||
+//     $("#input-integral-to")[0].classList.contains('is-invalid')
+//   var isToNow = $("#input-integral-to")[0].value
+//   var isToNow = isToNow.length == 0 || isToNow.toLowerCase() == 'now'
+//   if (!invalid) {
+//     viewerFetcher.integralBeginTime = beginTime
+//     viewerFetcher.integralEndTime = endTime
+//     viewerFetcher.changeMode(isToNow ? "IntegralContinues" : "Integral")
+//     if(isToNow) viewerFetcher.lastTime = dateToISO(endTime)
+//     viewerFetcher.range(beginTime, endTime)
+//   }
+// }
+//
+// function onSelectionIntegral(isIntegral) {
+//   $("#selection-instant").attr("class", isIntegral ? "btn btn-secondary" :
+//     "btn btn-success")
+//   $("#selection-integral").attr("class", isIntegral ? "btn btn-success" :
+//     "btn btn-secondary")
+//   $("#IntegralConfig").collapse(isIntegral ? "show" : "hide")
+//   viewerFetcher.changeMode(isIntegral ? "Stop" : "Instant")
+// }
+//
+// function onBlurIntegralRange(id) {
+//   element = $("#" + id)[0]
+//   text = element.value
+//   isNow = false
+//   if (text.length == 0 || text.toLowerCase() == "now") {
+//     parsedDate = new Date()
+//     isNow = (id == 'input-integral-to')
+//   } else parsedDate = parseSimpleDate(text)
+//   classList = element.classList
+//   if (parsedDate) {
+//     classList.remove('is-invalid')
+//     if (!isNow) element.value = dateToString(parsedDate)
+//   } else {
+//     classList.add('is-invalid')
+//   }
+//   return parsedDate
+// }
+//
+// function dateToISO(date) {
+//   return dateToString(date).replace(' ', 'T') + '.000000+08:00'
+// }
+//
+// function setFetchingProgress(p) {
+//   progress = parseInt(p * 100)
+//   $('#FetchingProgress').attr('style', 'background-image: linear-gradient(to right, #BDE6FF ' + (progress) + '%, #F8F9FC ' + (progress) + '%)')
+// }
--- InteractionFreePlatform/static/javascripts/IFExp.js
+++ InteractionFreePlatform/static/javascripts/IFExp.js
@@ -107,3 +107,171 @@ function dateToString(date) {
 String.prototype.replaceAll = function(s1, s2) {
   return this.replace(new RegExp(s1, "gm"), s2);
 }
+
+class TDCStorageStreamFatcher {
+  constructor(worker, collection, updateInterval, filter, ploter, listener) {
+    this.worker = worker
+    this.collection = collection
+    this.updateInterval = updateInterval
+    this.updateRangedResultInterval = 50
+    this.lastTime = null
+    this.mode = 'Instant' // modes: Instant, Integral, IntegralContinues
+    this.filter = filter
+    this.fetchID = 0
+    this.ploter = ploter
+    this.integralTime = 0
+    this.integralBeginTime = null
+    this.integralEndTime = null
+    this.integralMostRecentTime = null
+    this.integralFetchedDataCount = 0
+    this.integralTotalDataCount = 0
+    this.integralContinuesHasNew = false
+    this.listener = listener
+    this.rangedResultQueue = []
+    this.ploter(null, false)
+  }
+
+  start() {
+    this.update()
+    this.updateRangedResult()
+  }
+
+  update() {
+    var __this = this
+    if (this.mode == 'Instant' || this.mode == 'IntegralContinues') {
+      var fetchID = this.fetchID
+      worker.request("Storage", "latest", [this.collection, this.lastTime, this.filter], {},
+        function(result) {
+          if (fetchID == __this.fetchID) {
+            if (result != null) {
+              __this.lastTime = result['FetchTime'];
+              if (__this.mode == 'Instant' || __this.mode == 'IntegralContinues') {
+                __this.updateResult(result, fetchID, 'update')
+              }
+            }
+            if (__this.mode == 'IntegralContinues') {
+              __this.integralTime = parseInt((new Date().getTime() - __this.integralBeginTime.getTime())/1000)
+              if (result != null){
+                __this.integralTotalDataCount += 1
+                __this.integralFetchedDataCount += 1
+              }
+            }
+            __this.updateFetchingInfo()
+          }
+        },
+        function(error) {
+          console.log("Error: " + error)
+        }
+      )
+    }
+    this.updateFetchingInfo()
+    setTimeout(this.update.bind(this), this.updateInterval)
+  }
+
+  range(beginTime, endTime) {
+    var __this = this
+    var fetchID = this.fetchID
+    this.integralTime = parseInt((endTime.getTime() - beginTime.getTime())/1000)
+    worker.request("Storage", "range", [this.collection, this.dateToISO(beginTime), this.dateToISO(endTime), 'FetchTime', this.filter], {},
+      function(result) {
+        __this.integralTotalDataCount += result.length
+        for (var i = 0; i < result.length; i++) {
+          worker.request("Storage", "get", [__this.collection, result[i]['_id'], __this.filter], {},
+            function(result) {
+              __this.rangedResultQueue.push([fetchID, result])
+            },
+            function(error) {
+              console.log("Error: " + error)
+            })
+          __this.integralMostRecentTime = new Date();
+          __this.integralMostRecentTime.setTime(Date.parse(result[i]['FetchTime']))
+        }
+      },
+      function(error) {
+        console.log("Error: " + error)
+      }
+    )
+  }
+
+  updateRangedResult() {
+    if (this.rangedResultQueue.length > 0) {
+      var resultSet = this.rangedResultQueue.shift()
+      if (resultSet[0] == this.fetchID) {
+        this.updateResult(resultSet[1])
+        this.integralFetchedDataCount += 1
+        this.updateFetchingInfo()
+      }
+    }
+    setTimeout(this.updateRangedResult.bind(this), this.rangedResultQueue.length > 0 ? 0 : this.updateRangedResultInterval)
+  }
+
+  updateResult(result) {
+    this.ploter(result, this.mode != 'Instant')
+  }
+
+  updateFetchingInfo() {
+    // Check if display no data warning
+    var fetchTimeDelta = 0
+    if (this.mode == 'Instant') {
+      fetchTimeDelta = new Date().getTime() - Date.parse(this.lastTime)
+    } else if (this.mode == 'IntegralContinues') {
+      if (this.integralContinuesHasNew) {
+        fetchTimeDelta = new Date().getTime() - Date.parse(this.lastTime)
+      } else {
+        if (this.integralFetchedDataCount == this.integralTotalDataCount && this.integralMostRecentTime != null) {
+          fetchTimeDelta = new Date().getTime() - this.integralMostRecentTime.getTime()
+        }
+      }
+    }
+    if (this.mode == 'Integral' || this.mode == 'IntegralContinues') {
+      if (this.integralFetchedDataCount == 0) {
+        fetchTimeDelta = 0
+      }
+    }
+    this.listener('FetchTimeDelta', fetchTimeDelta);
+
+    // Set prograss
+    if (this.integralTotalDataCount > 0 && this.integralFetchedDataCount < this.integralTotalDataCount) {
+      this.listener('FetchingProgress', this.integralFetchedDataCount * 1.0 / this.integralTotalDataCount)
+    } else {
+      this.listener('FetchingProgress', 0.0)
+    }
+
+    // Set FetchNumber
+    if (this.mode == 'Integral' || this.mode == 'IntegralContinues') {
+      this.listener('FetchingNumber', [this.integralFetchedDataCount, this.integralTotalDataCount, this.integralTime])
+    } else {
+      this.listener('FetchingNumber', null)
+    }
+  }
+
+  changeMode(mode) {
+    if (mode == this.mode) return
+    if (mode != 'Instant' && mode != 'Integral' && mode !=
+      'IntegralContinues' && mode != 'Stop') {
+      console.log('Bad mode: ' + mode)
+      return
+    }
+    this.mode = mode
+    this.integralTime = 0
+    this.integralTotalDataCount = 0
+    this.integralFetchedDataCount = 0
+    this.integralContinuesHasNew = false
+    if (mode == 'Instant') this.lastTime = 0
+    if (mode != 'Stop') this.fetchID += 1
+    this.ploter(null, false)
+  }
+
+  updateIntegralData(beginTime, endTime, isToNow) {
+    this.integralBeginTime = beginTime
+    this.integralEndTime = endTime
+    this.changeMode("Stop")
+    this.changeMode(isToNow ? "IntegralContinues" : "Integral")
+    if(isToNow) this.lastTime = this.dateToISO(endTime)
+    this.range(beginTime, endTime)
+  }
+
+  dateToISO(date) {
+    return dateToString(date).replace(' ', 'T') + '.000000+08:00'
+  }
+}
